6aa75d304031d0ceb805c4c8f046478cd7b4f378
thanhluong3012 <luongtidai@gamil.com>
2024-11-21 15:09:29 +0700

    Chuyen thanh file .php

---
+++ View/Admin/device.php
@@ -0,0 +1,146 @@
+<!doctype html>
+<html lang="en">
+	<head>
+		<!-- Required meta tags -->
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+		<!-- Meta -->
+		<meta name="description" content="Best Bootstrap Admin Dashboards">
+		<meta name="author" content="Bootstrap Gallery" />
+		<link rel="canonical" href="https://www.bootstrap.gallery/">
+		<meta property="og:url" content="https://www.bootstrap.gallery">
+		<meta property="og:title" content="Admin Templates - Dashboard Templates | Bootstrap Gallery">
+		<meta property="og:description" content="Marketplace for Bootstrap Admin Dashboards">
+		<meta property="og:type" content="Website">
+		<meta property="og:site_name" content="Bootstrap Gallery">
+		<link rel="shortcut icon" href="assets/images/favicon.svg">
+
+		<!-- Title -->
+		<title>Bootstrap Admin Dashboards</title>
+		<!-- Animated css -->
+		<link rel="stylesheet" href="assets/css/animate.css">
+		<!-- Bootstrap font icons css -->
+		<link rel="stylesheet" href="assets/fonts/bootstrap/bootstrap-icons.css">
+		<!-- Main css -->
+		<link rel="stylesheet" href="assets/css/main.min.css">
+		<link rel="stylesheet" href="assets/vendor/overlay-scroll/OverlayScrollbars.min.css">
+
+	</head>
+
+	<body>
+
+		<div class="page-wrapper">
+
+			<!-- Sidebar wrapper start -->
+			<?php
+				include('sidebar.php');
+			?>
+			<!-- Sidebar wrapper end -->
+
+			<div class="main-container">
+				<?php
+					include('header.php');
+				?>
+
+                <div class="row" style="margin-left: 10px;">
+							<div class="col-12">
+								<div class="card">
+									<div class="card-body">
+										<div class="table-responsive">
+											<table class="table m-0">
+												<thead>
+													<tr>
+														<th>STT</th>
+														<th>Tên thiết bị</th>
+														<th>Loại thiết bị</th>
+														<th>Tình trạng</th>
+														<th>Hình ảnh</th>
+                                                        <th>Tùy Chỉnh</th>
+													</tr>
+												</thead>
+												<tbody>
+													<tr>
+														<td>1</td>
+														<td>Alia</td>
+														<td>Willams</td>
+														<td>+143-148-60985</td>
+														<td>+143-148-60985</td>
+														<td>
+                                                            <button type="button" class="btn btn-outline-success">Sửa</button>
+                                                            <button type="button" class="btn btn-outline-danger">Xóa</button>
+                                                        </td>
+													</tr>
+													<tr>
+														<td>2</td>
+														<td>Nathan</td>
+														<td>James</td>
+														<td>+143-148-60985</td>
+														<td>+143-148-60985</td>
+														<td>+278-119-88790</td>
+
+													</tr>
+													<tr>
+														<td>3</td>
+														<td>Kelly</td>
+														<td>Thomas</td>
+														<td>+143-148-60985</td>
+														<td>+143-148-60985</td>
+														<td>+125-117-88763</td>
+													</tr>
+													<tr>
+														<td>4</td>
+														<td>Steve</td>
+														<td>Smitth</td>
+														<td>+143-148-60985</td>
+														<td>+143-148-60985</td>
+														<td>+334-676-66530</td>
+													</tr>
+													<tr>
+														<td>5</td>
+														<td>Kevin</td>
+														<td>Oliver</td>
+														<td>+143-148-60985</td>
+														<td>+143-148-60985</td>
+														<td>+435-667-99808</td>
+													</tr>
+												</tbody>
+											</table>
+										</div>
+									</div>
+								</div>
+
+							</div>
+						</div>
+			    </div>
+		    </div>
+		<!-- Page wrapper end -->
+
+
+
+
+
+
+		<script src="assets/js/jquery.min.js"></script>
+		<script src="assets/js/bootstrap.bundle.min.js"></script>
+		<script src="assets/js/modernizr.js"></script>
+		<script src="assets/js/moment.js"></script>
+
+		<!-- *************
+			************ Vendor Js Files *************
+		************* -->
+
+		<!-- Overlay Scroll JS -->
+		<script src="assets/vendor/overlay-scroll/jquery.overlayScrollbars.min.js"></script>
+		<script src="assets/vendor/overlay-scroll/custom-scrollbar.js"></script>
+
+		<!-- Apex Charts -->
+		<script src="assets/vendor/apex/apexcharts.min.js"></script>
+		<script src="assets/vendor/apex/custom/sales/salesGraph.js"></script>
+		<script src="assets/vendor/apex/custom/sales/revenueGraph.js"></script>
+		<script src="assets/vendor/apex/custom/sales/taskGraph.js"></script>
+
+		<!-- Main Js Required -->
+		<script src="assets/js/main.js"></script>
+	</body>
+</html>
